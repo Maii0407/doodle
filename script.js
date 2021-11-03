93859b92a1ba5d3942ca3db6ce72beed2736c3e0
Akmal Izuddin <izuddin0407@gmail.com>
2021-11-03 09:55:04 +0800

    made a function that calls other function to make the dynamic grid more easier

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.getElementById('container');
 let row = document.getElementsByClassName('row');
 let pixel = document.getElementsByClassName('pixel');
 
-//make empty divs
+//make empty divs rows
 function makeDiv(x){
     for(i = 0; i < x; i++){
         let row = document.createElement('div');
@@ -12,8 +12,8 @@ function makeDiv(x){
     }
 }
 
-//generates a dynamic grid
-function makeGrid(pixNum){
+//generates columns and fill the grid
+function makePixel(pixNum){
     for(i = 0; i < row.length; i++){
         for(p = 0; p < pixNum; p++){
             let pixel = document.createElement('div');
@@ -23,5 +23,10 @@ function makeGrid(pixNum){
     }
 }
 
-makeDiv(16);
-makeGrid(16);
+//generates a grid
+function makeGrid(rowNum, colNum){
+    makeDiv(rowNum);
+    makePixel(colNum);
+}
+
+makeGrid(16, 16);
