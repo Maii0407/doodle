f7db765edfbeaf8fd90acc3c8b68b9bad1d4580a
Akmal Izuddin <izuddin0407@gmail.com>
2021-11-11 16:34:04 +0800

    made a function for the rainbow mode button to turn it on or off

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const container = document.getElementById('container');
 const clearBtn = document.getElementById('clearBtn');
 const setBtn = document.getElementById('setBtn');
 const rainbowBtn = document.getElementById('rainbowBtn');
-const shadingBtn = document.getElementById('shadingBtn');
 let pixel = document.getElementsByClassName('pixel');
+let rainbow = false;
 
 //event listeners
-container.addEventListener('mouseover', turnBlack);
+container.addEventListener('mouseover', colorGrid);
 clearBtn.addEventListener('click', clearGrid);
 setBtn.addEventListener('click', setGrid);
+rainbowBtn.addEventListener('click', rainbowClick);
 
 //generates grid 
 function makeGrid(rows,columns){
@@ -28,9 +29,14 @@ function createGrid(pixNum){
 }
 
 //function that colors the grid black
-function turnBlack(e){
-    e.target.style.backgroundColor = 'black';
-} 
+function colorGrid(e){
+    if(rainbow === true){
+        let randomRgb = '#'+Math.floor(Math.random()*16777215).toString(16);
+        e.target.style.backgroundColor = randomRgb;
+    } else{
+        e.target.style.backgroundColor = 'black'; 
+    }
+}
 
 //function that clears the grid and set the grid to default
 function clearGrid(e) {
@@ -42,9 +48,16 @@ function clearGrid(e) {
 
 //function for button to change the current grid
 function setGrid(){
-    container.innerHTML = '';
     let x = prompt('Pick any number, any number. Between 2 to 50:');
-    createGrid(x);
+    container.innerHTML = '';
+    if(x > 50){
+        alert('GRID TOO BIG T_T');
+        createGrid(16);
+    } else if(x === null){
+        createGrid(16);
+    } else{
+        createGrid(x);
+    }
 }
 
 //function to open navigation bar
@@ -57,4 +70,12 @@ function closeNav(){
     document.getElementById('sideNav').style.width = '0';
 }
 
+//check if the rainbow mode is on or off
+function rainbowClick(){
+    if(rainbow === false){
+        return rainbow = true;
+    } else{
+        return rainbow = false;
+    }
+}
 createGrid(16);
